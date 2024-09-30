8b58643e1acee8cf01f8c3af1e3f15fb2eab54ba
Piyush Saxena <piyush.saxena@globalvoxinc.com>
2024-10-01 00:02:45 +0530

    On branch feature/remove-unnessary-code  - clean up redux

--- src/redux/user/reducer.user.js
+++ src/redux/user/reducer.user.js
@@ -1,33 +1,13 @@
 import { createReducer } from '@reduxjs/toolkit';
-import {
-  LOADING_KEYS,
-  setAuctionInfo,
-  setAvatarList,
-  setLoading,
-  setUserInfo,
-} from './action.user';
+import { LOADING_KEYS, setLoading } from './action.user';
 
 const initialState = {
   [LOADING_KEYS.LOGIN_LOADER]: false,
-  [LOADING_KEYS.AVATAR_LIST_LOADER]: false,
-  auctionInfo: null,
-  userInfo: null,
-  avatarList: [],
 };
 
 export const userReducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(setLoading, (state, { payload }) => {
-      const { key, value } = payload;
-      state[key] = value;
-    })
-    .addCase(setAuctionInfo, (state, { payload }) => {
-      state.auctionInfo = payload;
-    })
-    .addCase(setUserInfo, (state, { payload }) => {
-      state.userInfo = payload;
-    })
-    .addCase(setAvatarList, (state, { payload }) => {
-      state.avatarList = payload;
-    });
+  builder.addCase(setLoading, (state, { payload }) => {
+    const { key, value } = payload;
+    state[key] = value;
+  });
 });
